545d7265f5a8d7fe97356dade696e8c9c48817b3
Isaiah Carter <izay.carter0509@gmail.com>
2019-10-16 10:47:07 -0400

    fixed dj data base

--- conf/settings.py
+++ conf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -92,7 +93,7 @@ if DEBUG:
     }
 else:
     DATABASES = {
-        'default': dj_database.url.config(default=os.environ['DATABASE_URL']),
+        'default': dj_database_url.config(default=os.environ['DATABASE_URL']),
     }
 
 
